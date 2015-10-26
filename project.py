52e1e23c1284793d80b44b4413dcf9c3d293a5d1
Pavel Raiskup <praiskup@redhat.com>
2015-10-26 11:48:42 +0100

    build: cleanup a bit, reflect up2date cont-lib
    * cont-lib: Sync with upstream.
    * docker.yml (parts/commands): Remove symlink creation, it is done
    automatically by container-build.  Use container_build macro.
    (parts/footer): Use container_entrypoint macro.
    * project.py: Define $container_cmd macro. macro.

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ config = {
     'name': 'cont-postgresql',
     'macros': {
         'cont_script_header': 'cont_debug "command: $0 $*"',
+        'container_cmd': '$bindir/cont-postgresql-cmd',
         'pgdata': '/var/lib/pgsql/data',
         'pghome': '/var/lib/pgsql',
         'atomic_install':
